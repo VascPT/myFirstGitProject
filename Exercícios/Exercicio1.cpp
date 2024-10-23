3209864593f474e91f7cd269c9037bd78b8f1f96
VascPT <vascorf2008@gmail.com>
2024-10-23 10:12:32 +0000

    exercicio

--- Exercícios/Exercicio1.cpp
+++ Exercícios/Exercicio1.cpp
@@ -1,1 +1,16 @@
 #include <iostream>
+using namespace std;
+
+int main(){
+
+    int Número;
+
+    cout << "Digite um número: " << endl;
+    cin >> Número;
+
+    if(Número%2 == 0){
+        cout << "Par" << endl;
+    }else{
+        cout << "Ímpar" << endl;
+    }
+    }
