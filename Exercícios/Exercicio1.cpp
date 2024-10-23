f6ca373b743e608a5654a49cbf609fc211957fd0
VascPT <vascorf2008@gmail.com>
2024-10-23 09:43:38 +0000

    exercicios

---
+++ Exercícios/Exercicio1.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    char vogal;
+
+    cout << "Introduza uma vogal: " << endl;
+    cin >> vogal;
+    
+    if(vogal == 'a' || vogal == 'e' || vogal == 'i' || vogal == 'o' || vogal == 'u'){
+        cout << "A vogal digitada é: " << vogal << endl;
+    }else{ 
+        cout << "A letra digitada não é uma vogal" << endl;
+    }
+    }
+    //switch(vogal){
+      //  case 'a':
+        //cout << "A vogal é: (a)(A)" << endl;
+        //break;
+        //case 'e':
+        //cout << "A vogal é: (e)(E)" << endl;
+        //break;
