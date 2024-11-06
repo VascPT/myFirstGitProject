72356d296c6b21c5465f8b7e26b55d35be3c7cd7
VascPT <vascorf2008@gmail.com>
2024-11-06 10:31:34 -0100

    Add files via upload

---
+++ TPCenunciado.cpp
@@ -0,0 +1,4 @@
+/*-Um programa que peça uma medida em metros e converta para milimetros
+-Um programa que peça o raio de um circulo e apresente o seu perimetro e a sua área
+-Um programa que vos peça o valor/hora de um trabalhador, o número de horas trabalhadas e apresente o valor total a pagar
+pelas horas trabalhadas. E o desconto de 12.5% para o IRS.
