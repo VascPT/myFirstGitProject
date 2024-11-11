50ecb921b55b28b9df8cc9157a1dc613a935e0a9
VascPT <vascorf2008@gmail.com>
2024-11-11 14:23:01 -0100

    Add files via upload

---
+++ Convertor de metros.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+int main(){
+    cout << "Quantidade de metros para converter?: ";
+    float metros;
+    cin >> metros;
+    cout << metros << "mt são " << metros*1000 << "mm" << endl;
+}
