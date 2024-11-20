d1254a79dc365178396f976cb9aa68356a44e049
VascPT <vascorf2008@gmail.com>
2024-11-20 10:30:26 -0100

    Add files via upload

---
+++ contador.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main(){
+    //vou fazer um contador que começa a contar no número 1 e vai acabar no número 5 com incrementos de 1 em 1
+    int num[5] = {1,2,3,4,5};
+    for(int num=1 ; num<=5 ; num++){
+        cout << num;
+    }
+    return 0;
+}
