3fb5793b1d0cb7e4f9476bdece4afe33024178b5
VascPT <vascorf2008@gmail.com>
2024-12-02 14:23:02 -0100

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+void bemVindo(){
+    cout << "Bem-vindo ao mundo das funções!" << endl;
+    
+}
+int main(){
+    
+    
+    return 0;
+}
