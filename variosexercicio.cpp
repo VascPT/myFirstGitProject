b560c4b3d41680933d24d2dee1a1b356388e55bf
VascPT <vascorf2008@gmail.com>
2024-12-16 15:29:03 -0100

    Add files via upload

---
+++ variosexercicio.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include <unistd.h>
+using namespace std;
+
+int main(){
+    //string nome;
+    //int ano;
+    //int numero;
+    //cout << "Qual é o teu nome? ";
+    //cin >> nome;
+    //cout << "Olá, " << nome << "!" << endl;
+    
+    //cout << "Qual foi o ano que nasceste? ";
+    //cin >> ano;
+    //cout << "Você tem " << 2024 - ano << " anos." << endl;
+    
+    //for(int i=1; i<=10; i++) cout << i << "\t";
+    //cout << endl;
+    //cout << "Qual é tabuada que queres? ";
+    //cin >> numero;
+    //for(int i=1; i<=10; i++) cout << numero << " x " << i << " = " << numero * i << endl;
+    
+    //int largura, altura;
+    //cout << "Digite a largura: ";
+    //cin >> largura;
+    //cout << "Digite a altura: ";
+    //cin >> altura;
+    //rotina para desenhar um retangulo com largura e altura informados
+    //for(int i=0; i<altura; i++){
+      //  for(int j=0; j<largura;j++) cout << "* ";
+        //cout <<endl;
+    //}
+    //TPC: em vez de um retângulo, um triângulo ^^^
+    return 0;
+}
