b227f2d6285b492df9d35d44ec22baf86a2f31a8
VascPT <vascorf2008@gmail.com>
2025-02-27 09:58:09 -0100

    Add files via upload

---
+++ calculo energia pt.2.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+//#define CONSTANTE_PLANK 6.62607015e-34 //e = a um numero elevado
+
+//float calculoEnergiaFotoes(float f, float h=CONSTANTE_PLANK){
+//    return h*f;
+//}
+
+//float calculoVelocidadeLuz(float f, float lambda){
+//    return f*lambda;
+//}
+
+
+
+int main(){
+    system("clear");
+    //float f;
+    //cout << "Frequência: ";
+    //cin >> f;
+    //cout << "Energia: " << calculoEnergiaFotoes(f) << endl;
+    
+    //float f, lambda;
+    //cout << "Frequência: ";
+    //cin >> f;
+    //cout << "Comprimento de onda: ";
+    //cin >> lambda;
+    //cout << "Velocidade da luz: " << calculoVelocidadeLuz(f, lambda) << endl;
+    
+    
+    
+    return 0;
+}
