4b962cb0f2b617d4855d3e071ad1210153c6b341
Vasco <vascorf2008@gmail.com>
2025-05-01 23:25:17 +0100

    Stuff/Testtest123

--- Stuff/Testtest123.cpp
+++ Stuff/Testtest123.cpp
@@ -2,6 +2,28 @@
 using namespace std;
 
 int main(){
-    cout << "I am steve" << endl;
+    float num;
+    cout << "Write a number: ";
+    cin >> num;
+   do{
+    if(num>0){
+        cout << "Here is the result of your number times two: " << num*2 << endl;
+    }else{
+        cout << "Number has to be higher than 0! Write another number: ";
+        cin >> num;
+    }
+}while (num <= 0);
+    float num2;
+    cout << endl << "Now tell me another number: ";
+    cin >> num2;
+    do{
+    if(num2>0){
+    cout << "Here is the division of your second number by 5: " << num2 / 5 << endl;
+    }else{
+        cout << "Number has to be higher than 0! Write another number: ";
+        cin >> num2;
+    }
+}while (num2 <= 0);
+    
     return 0;
 }
